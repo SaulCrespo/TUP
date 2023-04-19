f4960a6b90efbf0ba328630168fdb73167870884
Saul <saul.crespo.sc@gmail.com>
2023-04-19 15:52:56 +0200

    Inicial

---
+++ tup.bat
@@ -0,0 +1,12 @@
+@ECHO Off
+
+ECHO Inicializando proyecto...
+
+cd C:\users\saulc\Documents\VSCode
+
+start code TUP
+start winword TUP\src\doc\bitacora.docx
+start chrome https://dreamstudio.ai/generate https://chat.openai.com/
+start Photoshop
+exit
+
