1628253ad9ed964a88412315e2c286872f2856c2
Saul <saul.crespo.sc@gmail.com>
2023-04-27 10:14:03 +0200

    Ajustes al estilo del header y de la página

--- tup.bat
+++ tup.bat
@@ -6,6 +6,6 @@ cd C:\users\saulc\Documents\VSCode
 
 start code TUP
 start winword TUP\src\doc\bitacora.docx
-start chrome https://dreamstudio.ai/generate https://chat.openai.com/
+start chrome https://dreamstudio.ai/generate https://chat.openai.com/ https://www.canva.com/design/DAFhR5HrKL0/vPPJnXoCPpC9bFu4KIK5WA/edit
 exit
 
